f3c488d1cfe32a4c5c0c6f2bc216d398e6d04a5f
PedroAlcaide <Alcaide@MacBook-Pro-de-Pedro.local>
2015-05-25 15:41:38 -0300

    Teste1

---
+++ Horta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Horta
+//
+//  Created by Pedro Alcaide on 25/05/15.
+//  Copyright (c) 2015 Pedro Alcaide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
