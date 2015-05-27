9e7b8f26494c0ce93d64938399906041bd029b66
Felipe R. de Luca <felipe.rdeluca@gmail.com>
2015-05-27 18:49:15 -0300

    Framework do Facebook instalado.

---
+++ Horta/Modelos/User/LoginController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginController.swift
+//  Horta
+//
+//  Created by Felipe R. de Luca on 5/27/15.
+//  Copyright (c) 2015 Pedro Alcaide. All rights reserved.
+//
+
+import Foundation
+
+class LoginController {
+    
+}
