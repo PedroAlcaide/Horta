ad833afebef38d0652e44d2f6c9be574d400d42e
Felipe R. de Luca <felipe.rdeluca@gmail.com>
2015-05-28 14:54:17 -0300

    Implementação da classe controladora de login e alteração do Bundle Identifier do projeto para Senac.BEPiD.Challenge.02.

--- Horta/Modelos/User/LoginController.swift
+++ Horta/Modelos/User/LoginController.swift
@@ -7,7 +7,38 @@
 //
 
 import Foundation
+import FBSDKCoreKit
+import FBSDKLoginKit
 
+//------------------------------------------------------------------------------------------
 class LoginController {
+    private let usuarioLogado: Bool;
+    
+    
+    init(){
+        usuarioLogado = false;
+    }
+    //------------------------------------------------------------------------------------------
+    final func conectarUsuario( facebook: Bool, google: Bool, nome: String?, senha: String? ) -> ( nome: String?, email: String? ){
+        
+        var nomeUsuario : String?;
+        var emailUsuario: String?;
+        
+        if facebook {
+            // Conectar usando facebook
+        }
+        else if google {
+            // Conectar usando gmail, google+
+            
+        }
+        else if ( nome != nil && senha != nil ) {
+            // Conectar através do próprio aplicativo
+        }
+        
+        
+        return ( nomeUsuario, emailUsuario );
+    }
+    //------------------------------------------------------------------------------------------
     
 }
+//------------------------------------------------------------------------------------------
